8a1bd366d0b2a4a68a98a2431fa0c0104c97d817
Alena Karenina <karena1q84@gmail.com>
2024-01-04 18:05:23 +0500

    refactor: add styled-components

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,8 +1,37 @@
-import React from "react";
 import PageWrapper from "./../layout/page-wrapper/page-wrapper";
 import features from "../../mocks/features";
-import "./style.css";
+import { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+  html {
+    height: 100%;
+  }
+
+  body,
+  html {
+    margin: 0;
+  }
+
+  body {
+    position: relative;
+    min-height: 100%;
+    font-family: ${(props) => props.theme.fontFamily};
+    font-size: ${(props) => props.theme.fontSizeDefault};
+    line-height: 27px;
+    font-weight: 400;
+    color: ${(props) => props.theme.colorBlackForText};
+  }
+
+  #root {
+    width: 100%;
+  }
+`;
 
 export default function App() {
-  return <PageWrapper features={features} />;
+  return (
+    <>
+      <GlobalStyle />
+      <PageWrapper features={features} />
+    </>
+  );
 }
--- src/index.tsx
+++ src/index.tsx
@@ -2,9 +2,13 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './components/app/app.tsx'
 import './index.css'
+import { ThemeProvider } from 'styled-components';
+import { defaultTheme } from './thems/default';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider theme={defaultTheme}>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
